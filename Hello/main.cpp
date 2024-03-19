7ef22d3ca21d380efd0250da8c2eb1ca16711789
Leon <Leon@LEWIS>
2024-03-19 17:25:56 +0800

    Added zzz

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -7,6 +7,8 @@ int main() {
 	cout << "Hello, world!" << endl;
 	cout << "yyyyyy!" << endl;
 	cout << "Hello, world!" << endl;
+	cout << "zzzzz!" << endl;
+	cout << "Hello, world!" << endl;
 	cout << "Hello, world!" << endl;
 	cout << "xxxxxx!" << endl;
 
