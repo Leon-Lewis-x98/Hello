f3a38efc873acd5a2c4395332d891251d0181146
Leon <Leon@LEWIS>
2024-03-19 17:18:22 +0800

    Del 2H

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int main() {
 	cout << "Hello, world!" << endl;
 	cout << "Hello, world!" << endl;
-	cout << "Hello, world!" << endl;
-	cout << "Hello, world!" << endl;
 	cout << "world!" << endl;
 	cout << "Hello, world!" << endl;
 	cout << "xxxxxx!" << endl;
