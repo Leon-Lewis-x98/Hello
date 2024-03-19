627e7ab9faef03ba07b49d366be6f16088f855d5
Leon <Leon@LEWIS>
2024-03-19 17:26:40 +0800

    Del many things

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -4,13 +4,6 @@ using namespace std;
 int main() {
 	cout << "Hello, world!" << endl;
 	cout << "Hello, world!" << endl;
-	cout << "world!" << endl;
-	cout << "Hello, world!" << endl;
-	cout << "yyyyyy!" << endl;
-	cout << "Hello, world!" << endl;
-	cout << "zzzzz!" << endl;
-	cout << "Hello, world!" << endl;
-	cout << "Hello, world!" << endl;
 	cout << "xxxxxx!" << endl;
 
 	return 0;
