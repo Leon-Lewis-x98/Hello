aa1ce2c930fbf823d7e4fca6cce688dfb65aabb0
Leon <Leon@LEWIS>
2024-03-19 17:25:00 +0800

    Resolve

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -4,13 +4,10 @@ using namespace std;
 int main() {
 	cout << "Hello, world!" << endl;
 	cout << "Hello, world!" << endl;
-<<<<<<< HEAD
 	cout << "world!" << endl;
-=======
 	cout << "Hello, world!" << endl;
 	cout << "yyyyyy!" << endl;
 	cout << "Hello, world!" << endl;
->>>>>>> branch1
 	cout << "Hello, world!" << endl;
 	cout << "xxxxxx!" << endl;
 
